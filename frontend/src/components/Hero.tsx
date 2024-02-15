b19cb22dc92cb9400c29d209755bde3022fea297
goodluck <goodluckkumar04@gmail.com>
2024-02-15 12:37:19 +0530

    hero section

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import hero from "../assets/hero.png"
+type Props = {}
+
+export default function Hero({}: Props) {
+  return (
+    <div>
+        <img src={hero} className='w-full max-h-[600px] object-cover' alt="" />
+    </div>
+  )
+}
