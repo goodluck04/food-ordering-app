ecf130ea8b827762bacaae947ac925a5c7b36fac
goodluck <goodluckkumar04@gmail.com>
2024-02-16 17:37:38 +0530

    clodinary setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,27 +2,32 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/myUserRoutes"
-
+import myUserRoute from "./routes/myUserRoutes";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.DB_URI as string).then(() => {
   console.log("Connected to db!");
 });
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin:"*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello!" });
 });
 
-
-app.use("/api/my/user",myUserRoute);
-
-
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("Server Started on localhost:7000");
